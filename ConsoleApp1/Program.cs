27c931637253c0079280076b80cc20cb9fe99d65
Justin Hung <justin.hung@asuscloud.com>
2022-08-11 22:52:37 +0800

    feat:test commit 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,16 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("TEST commit message");
 
+
 Console.WriteLine("Test");
 Console.WriteLine("Test");
 Console.WriteLine("Test");
 Console.WriteLine("Test");
 Console.WriteLine("Test");
-Console.WriteLine("You are a pig")
+Console.WriteLine("You are a pig");
+
+Console.WriteLine("You are a pig");
+
+
+Console.WriteLine("new commit");
+
